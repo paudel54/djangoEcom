8aa61aba4326a111524326f3f93c317c803f84dd
paudel54 <2sansrit@gmail.com>
2023-12-19 15:01:21 +0545

    implemented product star

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import Header from './components/Header'
+import Footer from './components/Footer'
+import { Container } from 'react-bootstrap'
+
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
   return (
     <div>
-      Hello
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome</h1>
+          <HomeScreen />
+        </Container>
+      </main>
+      <Footer />
     </div >
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,20 @@
+import { Container, Row, Col } from 'react-bootstrap'
+
+function Footer() {
+    return (
+
+        <footer>
+            <Container>
+                <Row>
+                    <Col className='text-center py-3'>
+                        Copyright &copy; DShop
+                    </Col>
+                </Row>
+            </Container>
+            Footer
+        </footer>
+
+    )
+}
+
+export default Footer
